efef80b1bf2d260a962bfaf3bff41328b1193c86
davidalantodd <david.todd@multiverse.io>
2024-01-09 06:51:34 -0500

    add GET/{id} route in musical controller

--- Controllers/MusicalController.cs
+++ Controllers/MusicalController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//route points to "api/Musical" -- whatever the name of the Controller class is minus "Controller"
 [Route("API/[Controller]")]
 [ApiController]
 public class MusicalController: ControllerBase
@@ -20,15 +21,35 @@ public class MusicalController: ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Musical>>> GetMusicals()
     {
+        if (_context.Musicals == null)
+        {
+            return NotFound();
+        }
         return await _context.Musicals.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Musical>>
+    GetMusical(long id)
+    {
+        if (_context.Musicals == null)
+        {
+            return NotFound();
+        }
+        var musical = await _context.Musicals.FindAsync(id);
+        if (musical == null)
+        {
+            return NotFound();
+        }
+        return musical;
     }
 
     [HttpPost]
     public async Task<ActionResult<Musical>> PostMusical(Musical musical)
     {
         _context.Musicals.Add(musical);
-        await _context.SaveChangesAsync();
 
+        await _context.SaveChangesAsync();
         return CreatedAtAction("GetMusical", new {id = musical.Id}, musical);
     }
 
